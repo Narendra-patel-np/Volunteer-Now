73fae9bbd06bea917c2e636b6255c774c3b78a0e
narendra-patel-np <narendrapatel.np70@gmail.com>
2023-12-18 17:44:54 +0530

    opportunities

--- frontend/src/components/routes/PrivateRoutes.jsx
+++ frontend/src/components/routes/PrivateRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 // import { useSelector } from 'react-redux';
 import { Navigate } from "react-router-dom";
-import { auth } from "../pages/login";
+// import { auth } from "../pages/login";
 
 const PrivateRoute = ({ children }) => {
-  return <div>{auth ? children : <Navigate to="/login" />}</div>;
+  let token = localStorage.getItem("token");
+  return <div>{token ? children : <Navigate to="/login" />}</div>;
 };
 
 export default PrivateRoute;
