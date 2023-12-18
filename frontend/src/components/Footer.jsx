916934ee583e866a0409c2ed9ef54804b14dc302
narendra-patel-np <narendrapatel.np70@gmail.com>
2023-12-18 12:02:27 +0530

    login register ui

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,176 +1,181 @@
-import React from 'react'
-import logo from "./images/Volnow.png"
-import { Button, Heading } from '@chakra-ui/react'
-import styled from 'styled-components'
-import { FaFacebook, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa"
+import React from "react";
+import logo from "./images/Volnow.png";
+import { Button, Heading } from "@chakra-ui/react";
+import styled from "styled-components";
+import { FaFacebook, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
-    return (
-        <DIV>
-         <hr />   
-        <div className='main'>
-            <div className='first'>
-                <div className='logo'>
-                    <img src={logo} alt="logo"/>
-                    <Heading size={"md"} mt={"30px"}>Volunteer-Now</Heading>
-                </div>
-                <p>Working with others, in a spirit of generosity and mutual respect, we want to help build a world where all people can lead free and dignified lives.</p>
-                <Button colorScheme='blue'>Donate to Volnow</Button>
-            </div>
-            <div className='middle' ></div>
-            <div className='third'>
-                <div>
-                    <h4>Discover</h4>
-                    <p>Jobs</p>
-                    <p>Internships</p>
-                    <p>Organizations</p>
-                    <p>Volunteer Opportunities</p>
-                    <p>Grad Schools</p>
-                    <p>Site Map</p>
-                </div>
-                <div>
-                    <h4>Post a Listing</h4>
-                    <p>Post a Job</p>
-                    <p>Sign Up</p>
-                    <p>Contact US</p>
-                    <p>For Employers</p>
-                    <p>For Grad Schools</p>
-                </div>
-                <div>
-                    <h4>Resources</h4>
-                    <p>Job Seeker Tools</p>
-                    <p>Career Advice</p>
-                    <p>Grad Resources</p>
-                    <p>Ideas for Action</p>
-                    <p>Logo & Design</p>
-                    <p>Newsletter</p>
-                </div>
-                <div>
-                    <h4>Learn More</h4>
-                    <p>About US</p>
-                    <p>Our Team</p>
-                    <p>Help & FAQ</p>
-                    <p>Contact Us</p>
-                    <p>Terms of Service</p>
-                    <p>Privacy Policy</p>
-                </div>
-            </div>
+  return (
+    <DIV>
+      <hr />
+      <div className="main">
+        <div className="first">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <Heading size={"md"} mt={"30px"}>
+              Volunteer-Now
+            </Heading>
+          </div>
+          <p>
+            Working with others, in a spirit of generosity and mutual respect,
+            we want to help build a world where all people can lead free and
+            dignified lives.
+          </p>
+          <Button colorScheme="blue">Donate to Volnow</Button>
         </div>
-        <div className='botttom'>
-             <div className='icons'>
-                    <FaTiktok className='icon'/>
-                    <FaInstagram className='icon'/>    
-                    <FaFacebook className='icon'/>
-                    <FaTwitter className='icon'/>
-             </div>
-             <div>
-                <p>Copyright C 2023 Volnow.org</p>
-             </div>
-             <div>
-                <p>English | Espanol | Portugues</p>
-             </div>
+        <div className="middle"></div>
+        <div className="third">
+          <div>
+            <h4>Discover</h4>
+            <p>Jobs</p>
+            <p>Internships</p>
+            <p>Organizations</p>
+            <p>Volunteer Opportunities</p>
+            <p>Grad Schools</p>
+            <p>Site Map</p>
+          </div>
+          <div>
+            <h4>Post a Listing</h4>
+            <p>Post a Job</p>
+            <p>Sign Up</p>
+            <p>Contact US</p>
+            <p>For Employers</p>
+            <p>For Grad Schools</p>
+          </div>
+          <div>
+            <h4>Resources</h4>
+            <p>Job Seeker Tools</p>
+            <p>Career Advice</p>
+            <p>Grad Resources</p>
+            <p>Ideas for Action</p>
+            <p>Logo & Design</p>
+            <p>Newsletter</p>
+          </div>
+          <div>
+            <h4>Learn More</h4>
+            <p>About US</p>
+            <p>Our Team</p>
+            <p>Help & FAQ</p>
+            <p>Contact Us</p>
+            <p>Terms of Service</p>
+            <p>Privacy Policy</p>
+          </div>
         </div>
-        </DIV>
-    )
+      </div>
+      <div className="botttom">
+        <div className="icons">
+          <FaTiktok className="icon" />
+          <FaInstagram className="icon" />
+          <FaFacebook className="icon" />
+          <FaTwitter className="icon" />
+        </div>
+        <div>
+          <p>Copyright C 2023 Volnow.org</p>
+        </div>
+        <div>
+          <p>English | Espanol | Portugues</p>
+        </div>
+      </div>
+    </DIV>
+  );
 }
 
 const DIV = styled.div`
-    .main{
-        /* background-color: #04152D; */
-        /* border: 2px solid green; */
-        width: 86%;
-        margin: auto;
-        display: flex;
-        gap: 40px; 
-        margin-top: 80px;
-        /* color: white; */
-    }
-    .middle{
-        border:0.5px solid lightgray;
-        transform:rotate(180deg);
-        height: 300px 
-    }    
-    .first{
-        /* border:1px solid black; */
-        width:300px;
-        display: grid;
-        text-align: left;
-        gap: 30px;
-    }
-    .logo{
-        display: flex; 
-        /* margin-left: 80px; */
-    }
-    .logo img{
-        width: 100px;
-        border-radius: 50%;
-    }
-    .icons{
-        /* border:1px solid black; */
-        display: flex;
-        gap: 30px;
-    }
-    .icon{
-        width: 40px;
-        height: 30px;
-    }
-    .icon:hover{
-        color: #3d3dd4;
-    }
-    .third{
-        display: flex;
-        gap: 20px;
-        margin: 0 auto 0 auto;
-        text-align: left;
-    }
-    .third div h4{
-        font-weight: bold;
-    }
-    .third div{
-        display: grid;
+  .main {
+    /* background-color: #04152D; */
+    /* border: 2px solid green; */
+    width: 86%;
+    margin: auto;
+    display: flex;
+    gap: 40px;
+    margin-top: 80px;
+    /* color: white; */
+  }
+  .middle {
+    border: 0.5px solid lightgray;
+    transform: rotate(180deg);
+    height: 300px;
+  }
+  .first {
+    /* border:1px solid black; */
+    width: 300px;
+    display: grid;
+    text-align: left;
+    gap: 30px;
+  }
+  .logo {
+    display: flex;
+    /* margin-left: 80px; */
+  }
+  .logo img {
+    width: 100px;
+    border-radius: 50%;
+  }
+  .icons {
+    /* border:1px solid black; */
+    display: flex;
+    gap: 30px;
+  }
+  .icon {
+    width: 40px;
+    height: 30px;
+  }
+  .icon:hover {
+    color: #3d3dd4;
+  }
+  .third {
+    display: flex;
+    gap: 20px;
+    margin: 0 auto 0 auto;
+    text-align: left;
+  }
+  .third div h4 {
+    font-weight: bold;
+  }
+  .third div {
+    display: grid;
+  }
+  .third div p {
+    cursor: pointer;
+  }
+  .botttom {
+    width: 86%;
+    margin: auto;
+    justify-content: space-between;
+    display: flex;
+    margin-top: 40px;
+    margin-bottom: 20px;
+  }
 
+  @media screen and (max-width: 700px) {
+    .main {
+      display: flex;
+      flex-direction: column;
+      width: 86%;
+      margin: auto;
+      gap: 40px;
+      margin-top: 80px;
+      /* color: white; */
     }
-    .third div p{
-        cursor: pointer;
+    .first {
+      margin: auto;
     }
-    .botttom{
-        width: 86%;
-        margin: auto;
-        justify-content: space-between;
-        display: flex;
-        margin-top: 40px;
-        margin-bottom: 20px;
+    .middle {
+      display: none;
     }
-
-  @media screen and (max-width:700px) {
-    .main{
-        display: flex;
-        flex-direction: column;
-        width: 86%;
-        margin: auto;
-        gap: 40px; 
-        margin-top: 80px;
-        /* color: white; */
+    .botttom {
+      width: 86%;
+      margin: auto;
+      margin-top: 40px;
+      margin-bottom: 20px;
+      display: flex;
+      gap: 20px;
+      flex-direction: column;
     }
-    .first{
-        margin: auto;
+    .icons {
+      margin: auto;
+      display: flex;
+      gap: 30px;
     }
-    .middle{
-        display: none;
-    } 
-    .botttom{
-        width: 86%;
-        margin: auto;
-        margin-top: 40px;
-        margin-bottom: 20px;
-        display: flex;
-        gap: 20px;
-        flex-direction: column;
-    }
-    .icons{
-        margin: auto;
-        display: flex;
-        gap: 30px;
-    }
-  }   
-`
+  }
+`;
--- frontend/src/components/pages/login.jsx
+++ frontend/src/components/pages/login.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { Box, Button, Input, Stack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>login page</div>;
+  return (
+    <Box
+      w={"25%"}
+      m={"auto"}
+      mt={"120px"}
+      p={"12"}
+      bg={"#f5f5f5"}
+      borderRadius={"20"}
+    >
+      <Stack spacing={5}>
+        <Text
+          textAlign={"center"}
+          fontWeight={"bold"}
+          mb={2}
+          fontSize={"25"}
+          color={"#0b3954"}
+        >
+          Log in
+        </Text>
+
+        <Input
+          variant="filled"
+          placeholder="Email"
+          type="text"
+          name="email"
+          _placeholder={{ color: "black" }}
+          // isInvalid
+          // errorBorderColor="orange.400"
+          // onChange={handleChange}
+          // value={newProduct.category}
+        />
+        <Input
+          variant="filled"
+          placeholder="Password"
+          type="password"
+          name="password"
+          _placeholder={{ color: "black" }}
+
+          // isInvalid
+          // errorBorderColor="orange.400"
+          // value={newProduct.price}
+          // onChange={handleChange}
+        />
+
+        <Button
+          // #0b3954
+          bg={"orange.400"}
+          m={"20px 5px"}
+          color={"white"}
+          _hover={{ bg: "#e89f22" }}
+          // onClick={AddProduct}
+        >
+          Log In
+        </Button>
+      </Stack>
+      <Link style={{ textDecoration: "underline" }} to="/register">
+        Register
+      </Link>
+    </Box>
+  );
 };
 
 export default Login;
